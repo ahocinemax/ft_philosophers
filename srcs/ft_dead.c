2475df1540a198041bc8a3a77e1965dce90e41f8
Abdessalam HOCINE <ahocine@e1r7p16.clusters.42paris.fr>
2022-08-13 20:39:20 +0200

    A

--- srcs/ft_dead.c
+++ srcs/ft_dead.c
@@ -25,7 +25,7 @@ void	*ft_dead_timer(void *ptr)
 	{
 		pthread_mutex_unlock(arg->display);
 		time = ft_get_time();
-		usleep((arg->time_to_die - philo->last_meal) * 1000);
+		usleep((arg->time_to_die - time - philo->last_meal) * 1000);
 		time = ft_get_time();
 		pthread_mutex_trylock(arg->display);
 		if (time - philo->last_meal >= arg->time_to_die)
